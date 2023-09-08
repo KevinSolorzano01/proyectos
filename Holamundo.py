d9c1082ab07d1e6fa5aaf9f89b90643cef283928
kevinSolorzano <andreksg0101@gmail.com>
2023-09-08 12:28:38 -0500

    se agrega el proyecto

---
+++ Holamundo.py
@@ -0,0 +1,12 @@
+print('Hola') 
+print("Hola Mundo") #comando para imprimir por pantalla 
+
+
+# Define the release and runtime integer variables below:
+release_year = 1977
+runtime = 1980
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 6.8
+print(type(runtime))
+print(type(rating_out_of_10))
